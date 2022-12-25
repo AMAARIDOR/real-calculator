653a8314fb45b10527fa5470c15e42bbb6cbfaed
HZZRD <arish2k3@gmail.com>
2022-12-25 16:02:50 +0530

    add init variables

---
+++ app.js
@@ -0,0 +1,3 @@
+let calculatorDisplay = document.getElementById("calculator-display");
+let numberButtons = document.querySelectorAll(".number-button");
+let operatorButtons = document.querySelectorAll(".operator-button");
