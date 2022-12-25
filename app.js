10b76dec112e72a8696d89f92ccba4118fec7b0b
HZZRD <arish2k3@gmail.com>
2022-12-25 19:05:43 +0530

    final calculator without optimization

--- app.js
+++ app.js
@@ -9,42 +9,62 @@ const clearButton = document.getElementById("clear-button");
 // Arrays for number storage:
 
 let displayBoard = [];
-let firstNumber, secondNumber;
+let firstNumber, operator, secondNumber;
+
+// Calculator function:
+
+function calculate(a, op, b) {
+  switch (op) {
+    case "+":
+      return a + b;
+      break;
+    case "-":
+      return a - b;
+      break;
+    case "*":
+      return a * b;
+      break;
+    case "/":
+      return a / b;
+      break;
+  }
+}
 
 // Click Handling Function for numbers:
 
-let handleNumberClicks = () => {
-  numberButtons.forEach((numberButton) =>
-    numberButton.addEventListener("click", (event) => {
-      let clickedNumber = event.target.dataset.number;
-      displayBoard.push(clickedNumber);
-      calculatorDisplay.textContent = displayBoard.join("");
-    })
-  );
-};
+numberButtons.forEach((numberButton) =>
+  numberButton.addEventListener("click", (event) => {
+    let clickedNumber = event.target.dataset.number;
+    displayBoard.push(clickedNumber);
+    calculatorDisplay.textContent = displayBoard.join("");
+  })
+);
 
 // Click Handling Function for operators:
-
-let handleOperatorClicks = () => {
-  operatorButtons.forEach((operatorButton) =>
-    operatorButton.addEventListener("click", (event) => {
-      let clickedOperator = event.target.innerHTML;
-      calculatorDisplay.textContent += clickedOperator;
-      firstNumber = displayBoard.join("");
-      console.log(firstNumber);
-      displayBoard = [];
-    })
-  );
-};
+operatorButtons.forEach((operatorButton) =>
+  operatorButton.addEventListener("click", (event) => {
+    let clickedOperator = event.target.innerHTML;
+    calculatorDisplay.textContent += clickedOperator;
+    operator = clickedOperator;
+    firstNumber = displayBoard.join("");
+    console.log(firstNumber);
+    console.log(operator);
+    displayBoard = [];
+  })
+);
 
 // Equal button functionality:
 
 equalButton.addEventListener("click", () => {
   secondNumber = displayBoard.join("");
+  calculatorDisplay.textContent =
+    "=" + calculate(Number(firstNumber), operator, Number(secondNumber));
   console.log(secondNumber);
 });
 
-// Calling clickHandlers:
-
-handleNumberClicks();
-handleOperatorClicks();
+clearButton.addEventListener("click", () => {
+  displayBoard = [];
+  firstNumber = "";
+  secondNumber = "";
+  calculatorDisplay.textContent = "";
+});
